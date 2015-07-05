f83da6466735c3d64bc1e91ae542cee9b6905c8c
Buddy Magsipoc <keikun17@gmail.com>
2015-07-05 14:51:36 +0800

    Weapon style。

--- source/javascripts/bundle.js
+++ source/javascripts/bundle.js
@@ -21138,11 +21138,15 @@
 	
 	  styles: {
 	    display: 'block',
+	    border: '1px solid #e67e22',
 	    borderRadius: '4px',
-	    backgroundColor: '#e67e22',
+	    color: '#e67e22',
+	    cursor: 'pointer',
+	    // backgroundColor: 'transparent',
 	    padding: '10px 5px',
 	    marginTop: '3px',
-	    width: '200px',
+	    minWidth: '100px',
+	    maxWidth: '150px',
 	    textAlign: 'center'
 	  },
 	

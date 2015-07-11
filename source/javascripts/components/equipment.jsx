ff0f860f4ea47a5ffc8bb5b6cfea579def158bbe
Buddy Magsipoc <keikun17@gmail.com>
2015-07-11 22:50:56 +0800

    Heatsink as component。

--- source/javascripts/components/equipment.jsx
+++ source/javascripts/components/equipment.jsx
@@ -1,19 +1,15 @@
+var Heatsink = require('./heatsink')
+
 var Equipment = React.createClass({
   styles: {
     paddingLeft: '10px',
     flex: '1 0 0',
   },
+
   render: function(){
     return <equipments style={this.styles}>
       <h1>Equipments</h1>
-      <div className="equipment">
-        <label>Internal Heatsinks</label>
-        <input type="number" value="10"/>
-      </div>
-      <div>
-        <label>External Heatsinks</label>
-        <input type="number" value="0"/>
-      </div>
+      <Heatsink/>
     </equipments>
   }
 })
---
+++ source/javascripts/components/heatsink.jsx
@@ -0,0 +1,34 @@
+class Heatsink extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      internal: 8,
+      external: 5
+    }
+  }
+
+  render() {
+    return (
+      <heatsink>
+        <internal_heatsink>
+          <div className="equipment">
+            <label>Internal Heatsinks</label>
+            <input type="number" value={this.state.internal}/>
+          </div>
+        </internal_heatsink>
+
+        <external_heatsink>
+          <div className="equipment">
+            <label>Internal Heatsinks</label>
+            <input type="number" value={this.state.external}/>
+          </div>
+        </external_heatsink>
+      </heatsink>
+    )
+
+  }
+}
+
+
+export default Heatsink

1ef7da0e4b381e728fea867915ed8e73d389fda4
Buddy Magsipoc <keikun17@gmail.com>
2015-07-19 23:07:52 +0800

    Cooldown Algo。

--- source/javascripts/components/actions/cooldown_actions.jsx
+++ source/javascripts/components/actions/cooldown_actions.jsx
@@ -20,9 +20,15 @@ var CooldownActions = {
       external_heatsink_cooldown_modifier = .1
     }
 
+    var internal_cooldown = (internal_heatsink_cooldown_modifier * heatsink_store_data.internal_heatsinks)
+    var external_cooldown = (external_heatsink_cooldown_modifier * heatsink_store_data.external_heatsinks)
+
+    var time_to_zero = "WIP"
+    var cool_rate = internal_cooldown + external_cooldown
+
     AppDispatcher.dispatch({
       action_type: CooldownConstants.COOLDOWN_UPDATE,
-      new_data: {time_to_zero: 99, cool_rate: 99}
+      new_data: {time_to_zero: time_to_zero, cool_rate: cool_rate}
     })
   }
 

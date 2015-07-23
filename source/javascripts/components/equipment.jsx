fdf2b31c2544b263983a39376e0678756eeda21d
Buddy Magsipoc <keikun17@gmail.com>
2015-07-23 20:36:00 +0800

    Style fuckup fix。

--- source/javascripts/components/equipment.jsx
+++ source/javascripts/components/equipment.jsx
@@ -4,9 +4,10 @@ import WeaponStore from "./stores/weapon_store"
 import EquippedWeapon from "./equipped_weapon"
 
 class Equipment extends React.Component {
-  styles: {
-    paddingLeft: '10px',
-    flex: '1 0 0',
+  getStyle() {
+    return {
+      paddingLeft: '10px',
+      flex: '1 0 0' }
   }
 
   constructor(props) {
@@ -29,17 +30,10 @@ class Equipment extends React.Component {
 
     var equipped_weapons = this.state.equipped_weapons.map(function(weapon ,index ,equipped_weapons){
       let _weapon = weapon.props.weapon
-      console.log('weapon is')
-      console.log(weapon)
-      console.log('index is')
-      console.log(index)
-      console.log('equipped weapons is')
-      console.log(equipped_weapons)
-
       return <EquippedWeapon name={_weapon.name} id={_weapon.id} />
     })
 
-    return <equipments style={this.styles}>
+    return <equipments style={this.getStyle()}>
 
       <h1>Equipments</h1>
       <Heatsink/>

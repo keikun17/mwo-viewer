950770312240aeab1cbe070886734f4a31c600a9
Buddy Magsipoc <keikun17@gmail.com>
2015-08-05 00:13:19 +0800

    Un inline styles。

--- source/javascripts/components/weapon.jsx
+++ source/javascripts/components/weapon.jsx
@@ -3,24 +3,8 @@ var WeaponActions = require('./actions/weapon_actions')
 
 var Weapon = React.createClass({
 
-  styles: {
-    display: 'block',
-    border: "1px solid #e67e22",
-    borderRadius: '4px',
-    color: '#e67e22',
-    cursor: 'pointer',
-    // backgroundColor: 'transparent',
-    // fontFamily: "'Open Sans Condensed', sans-serif",
-    fontFamily: "'Roboto', sans-serif",
-    padding: '10px 5px',
-    marginTop: '3px',
-    minWidth: '100px',
-    maxWidth: '150px',
-    textAlign: 'center'
-  },
-
   render: function(){
-    return <weapon style={this.styles} onClick={this._onClick}>{this.props.weapon.name}</weapon>
+    return <weapon onClick={this._onClick}>{this.props.weapon.name}</weapon>
   },
 
   _onClick: function(){

a93c035724428a545b6549d2c7cd7cc46fcfeacd
Buddy Magsipoc <keikun17@gmail.com>
2015-08-21 23:32:55 +0800

    Extract LRM weapon data from game data files。

--- source/javascripts/components/weapons_list.jsx
+++ source/javascripts/components/weapons_list.jsx
@@ -3,6 +3,12 @@ var weapons_list = [
   {weapon_id: 123, ghost_limit: 6, name: 'Medium Laser', cooldown_time: 3, heat: 4, damage: 5, ghost_heat_group: 'mlas'},
   {weapon_id: 123, ghost_limit: 3, name: 'Large Laser', cooldown_time: 3.25, heat: 7, damage: 9, ghost_heat_group: 'llas'},
   {weapon_id: 123, ghost_limit: 3, name: 'Large Pulse Laser', cooldown_time: 3.25, heat: 7, damage: 11, ghost_heat_group: 'llas'},
+
+
+  {weapon_id: 123, ghost_limit: 3, name: 'LRM 5',  cooldown_time: 3.5, heat: 2, damage: 5,  ghost_heat_group: 'lrm'},
+  {weapon_id: 123, ghost_limit: 2, name: 'LRM 10', cooldown_time: 4,   heat: 4, damage: 10, ghost_heat_group: 'lrm'},
+  {weapon_id: 123, ghost_limit: 2, name: 'LRM 15', cooldown_time: 4.5, heat: 5, damage: 15, ghost_heat_group: 'lrm'},
+  {weapon_id: 123, ghost_limit: 2, name: 'LRM 20', cooldown_time: 6,   heat: 6, damage: 20, ghost_heat_group: 'lrm'},
 ] ;
 
 module.exports = weapons_list

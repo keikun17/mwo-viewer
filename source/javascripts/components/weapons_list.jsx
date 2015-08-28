11e461589869c2faa1a1eae28dd37f819b30eeea
Buddy Magsipoc <keikun17@gmail.com>
2015-08-29 03:14:25 +0800

    Caps weapons names。

--- source/javascripts/components/weapons_list.jsx
+++ source/javascripts/components/weapons_list.jsx
@@ -1,8 +1,8 @@
 var weapons_list = [
-  {weapon_id: 123, ghost_limit: 0, type: 'las', name: 'Small Laser', cooldown_time: 2.25, heat: 3, damage: 3},
-  {weapon_id: 123, ghost_limit: 6, type: 'las', name: 'Medium Laser', cooldown_time: 3, heat: 4, damage: 5, ghost_heat_group: 'mlas'},
-  {weapon_id: 123, ghost_limit: 3, type: 'las', name: 'Large Laser', cooldown_time: 3.25, heat: 7, damage: 9, ghost_heat_group: 'llas'},
-  {weapon_id: 123, ghost_limit: 3, type: 'las', name: 'Large Pulse Laser', cooldown_time: 3.25, heat: 7, damage: 11, ghost_heat_group: 'llas'},
+  {weapon_id: 123, ghost_limit: 0, type: 'las', name: 'SMALL LASER', cooldown_time: 2.25, heat: 3, damage: 3},
+  {weapon_id: 123, ghost_limit: 6, type: 'las', name: 'MEDIUM LASER', cooldown_time: 3, heat: 4, damage: 5, ghost_heat_group: 'mlas'},
+  {weapon_id: 123, ghost_limit: 3, type: 'las', name: 'LARGE LASER', cooldown_time: 3.25, heat: 7, damage: 9, ghost_heat_group: 'llas'},
+  {weapon_id: 123, ghost_limit: 3, type: 'las', name: 'LARGE PULSE LASER', cooldown_time: 3.25, heat: 7, damage: 11, ghost_heat_group: 'llas'},
 
   {weapon_id: 123, ghost_limit: 2, type: 'las', name: 'PPC',    cooldown_time: 4,    heat: 10, damage: 11, ghost_heat_group: 'ppc'},
   {weapon_id: 123, ghost_limit: 2, type: 'las', name: 'ER PPC', cooldown_time: 4,    heat: 15, damage: 11, ghost_heat_group: 'ppc'},

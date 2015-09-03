f45124573b032dad9a5a4a1ec12e59e12aee3ec6
Buddy Magsipoc <keikun17@gmail.com>
2015-09-03 23:45:58 +0800

    Clan UACs。

--- source/javascripts/components/weapons_list.jsx
+++ source/javascripts/components/weapons_list.jsx
@@ -49,9 +49,13 @@ var weapons_list = {
 
     {weapon_id: 123, ghost_limit: 0, type: 'bal', name: 'C-AC 2',   cooldown_time: .72, heat: 1,  damage: 2 },
     {weapon_id: 123, ghost_limit: 3, type: 'bal', name: 'C-AC 5',   cooldown_time: 1.8, heat: 1,  damage: 5},
-    {weapon_id: 123, ghost_limit: 0, type: 'bal', name: 'C-UAC 5',  cooldown_time: 1.66, heat: 1,  damage: 5 },
     {weapon_id: 123, ghost_limit: 2, type: 'bal', name: 'C-AC 10',  cooldown_time: 2.9,  heat: 2,  damage: 10, ghost_heat_group: 'cac10' },
     {weapon_id: 123, ghost_limit: 1, type: 'bal', name: 'C-AC 20',  cooldown_time: 4.72, heat: 6,  damage: 20, ghost_heat_group: 'cac20'},
+
+    {weapon_id: 123, ghost_limit: 0, type: 'bal', name: 'C-UAC 2',  cooldown_time: .72, heat: 1,  damage: 2 },
+    {weapon_id: 123, ghost_limit: 3, type: 'bal', name: 'C-UAC 5',  cooldown_time: 1.66, heat: 1,  damage: 5 },
+    {weapon_id: 123, ghost_limit: 2, type: 'bal', name: 'C-UAC 10',  cooldown_time: 2.5, heat: 3,  damage: 10, ghost_heat_group: 'cac10' },
+    {weapon_id: 123, ghost_limit: 1, type: 'bal', name: 'C-UAC 20',  cooldown_time: 4.00, heat: 7,  damage: 20, ghost_heat_group: 'cac20'},
 
     {weapon_id: 123, ghost_limit: 0, type: 'bal', name: 'MACHINE GUN', cooldown_time: .1, heat: 0,  damage: 0.08 },
     {weapon_id: 123, ghost_limit: 0, type: 'bal', name: 'C-GAUSS RIFLE', cooldown_time: 4, heat: 1,  damage: 15 },
